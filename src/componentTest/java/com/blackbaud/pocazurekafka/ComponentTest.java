6ae404226f1b32343cf097df94af620308615f06
Blackbaud-KevinFung <kevin.fung@blackbaud.com>
2017-08-09 11:34:20 -0500

    initialize postgres container

--- src/componentTest/java/com/blackbaud/pocazurekafka/ComponentTest.java
+++ src/componentTest/java/com/blackbaud/pocazurekafka/ComponentTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 
 @Documented
 @Inherited
@@ -17,6 +18,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @Target(ElementType.TYPE)
 @SpringApplicationConfiguration(classes = {Pocazurekafka.class, TestConfig.class})
 @WebAppConfiguration
+@Sql(scripts = "classpath:/db/test_cleanup.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @IntegrationTest({"server.port=10000", "management.port=10001"})
 @ActiveProfiles({"local", "componentTest"})
 public @interface ComponentTest {
--- src/main/java/com/blackbaud/pocazurekafka/Pocazurekafka.java
+++ src/main/java/com/blackbaud/pocazurekafka/Pocazurekafka.java
@@ -4,12 +4,14 @@ import com.blackbaud.boot.config.CommonSpringConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.ManagementWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan({"com.blackbaud.pocazurekafka", "com.blackbaud.boot.converters"})
 @ComponentScan("com.blackbaud.pocazurekafka")
 @EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
 public class Pocazurekafka extends CommonSpringConfig {
