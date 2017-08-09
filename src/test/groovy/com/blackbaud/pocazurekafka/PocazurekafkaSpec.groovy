b7825a092cbdb071ffe0c5e326c3c7bd13ef4822
Blackbaud-KevinFung <kevin.fung@blackbaud.com>
2017-08-09 11:34:19 -0500

    add basic test

---
+++ src/test/groovy/com/blackbaud/pocazurekafka/PocazurekafkaSpec.groovy
@@ -0,0 +1,11 @@
+package com.blackbaud.pocazurekafka
+
+import spock.lang.Specification
+
+class PocazurekafkaSpec extends Specification {
+
+    def "should have a basic test" () {
+        expect:
+        true
+     }
+}
